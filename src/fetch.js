9edfa3df6f46729bad08fc380d8a1c9598a1a83a
i-guerrero <isaacguerrero@pursuit.org>
2022-11-21 20:24:33 -0500

    nav bar

--- src/fetch.js
+++ src/fetch.js
@@ -1,12 +1,10 @@
-fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`)
+fetch(
+  `https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`
+)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data)
-    //   addToHistory();
+    console.log(data);
   })
   .catch((error) => {
     console.log("Error: ", error);
   });
-
-
-
