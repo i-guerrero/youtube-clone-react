03e0035e82cd816e905e87aa4f7bd7bcdbc6e040
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-11-27 15:35:22 -0500

    About Page

---
+++ src/Components/IsaacBio.jsx
@@ -0,0 +1,18 @@
+
+
+export default function IsaacBio(){
+    return (
+      <><div className="IsaacBio">
+      <div className='IsaacCard'>
+          <img className='biopic' src='./Assets/isaacjpg.jpeg' alt='Issac'/>
+      </div>
+        <strong>Isaac Guerrero </strong>
+        <p>Full Stack Software Engineer - Pursuit | Interests - DevOps, cloud computing, machine learning | Technologies - JavaScript, React, Express, PostgreSQL
+        </p>
+      </div>
+      <div className="logos">
+  
+        </div></>
+    );
+  };
+  
