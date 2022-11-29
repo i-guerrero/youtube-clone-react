618f0171712719a9f7d613b6d16a3b2448ceca22
cjs2408 <carlossuero@pursuit.org>
2022-11-28 20:30:34 -0500

    modal change CS NOW

--- src/Modal.js
+++ src/Modal.js
@@ -1,25 +1,7 @@
 import React from "react";
+import "./Modal.css";
 import ReactDom from "react-dom";
 
-const MODAL_STYLES = {
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  backgroundColor: "#FFF",
-  padding: "50px",
-  zIndex: 1000,
-};
-
-const OVERLAY_STYLES = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: "rgba(0, 0, 0, .7)",
-  zIndex: 1000,
-};
 
 export default function Modal({ open, children, onClose }) {
   if (!open) return null;
@@ -27,8 +9,8 @@ export default function Modal({ open, children, onClose }) {
   return (
     ReactDom.createPortal(
       <>
-        <div style={OVERLAY_STYLES} />
-        <div style={MODAL_STYLES}>
+        <div className= "overlay" />
+        <div className= "modal">
           <button onClick={onClose}>Close Modal</button>
           {children}
         </div>
