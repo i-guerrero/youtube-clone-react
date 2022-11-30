b0863962221603a471a5f591364741e2856b2dbd
jamesscott@pursuit.org <Jimmy@2019mbp.local>
2022-11-29 19:50:01 -0500

    LastDance

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,6 +13,12 @@ export default function About() {
         <div className="member1"><Carlos /></div>
         <div className="member2">< Isaac /></div>
         <div className="member3">< James /></div>
+
+        <div className='ProjectDesription'>
+        <p> We created a Youtube clone with use of React & Node.js <br /> This project was bootstrapped with Create React App. We used the Youtube API and React Youtube Package to provide functionality.
+        </p>
+        </div>
+        
     </div>
   )
 }
